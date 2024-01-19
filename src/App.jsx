d3e91392ee75b352c5d040e6f768ec991c2909af
VectoRaxo <julialberti666@gmail.com>
2024-01-19 21:00:31 +0100

    Maquetacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Grid, GridItem, Heading } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter, Button, Text, Box } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Button, IconButton, Text, Box } from '@chakra-ui/react'
 import { GoPlus } from "react-icons/go";
 import { BsThreeDots } from "react-icons/bs";
 import { Divider } from '@chakra-ui/react'
@@ -13,29 +13,37 @@ function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <Container>
-      <Grid templateColumns='repeat(3, 200px)' gap={3}>
+      <Container className='mainPage' >
+      <Grid templateColumns='repeat(3, 300px)' gap={3}>
 
         {/* TO DO */}
 
         <GridItem w='100%' h='100%' background={'lightgrey'} borderRadius={'1em'}>
-          <Box textAlign={'left'} margin={'1em'}>
+          <Box  margin={'1em'}>
             <Grid templateColumns='repeat(3, 33%)'>
-              <GridItem colStart={1} colEnd={3} >
+              <GridItem colStart={1} colEnd={3} textAlign={'left'}>
                 <Text fontSize={'20px'} as={'b'} >
                   To do
                 </Text>
               </GridItem>
-              <GridItem colStart={3} >
-                <Button alignContent={'right'}  leftIcon={<BsThreeDots />} colorScheme='teal' variant='ghost' />
+              <GridItem colStart={3} textAlign={'right'}>
+                <IconButton colorScheme='teal' variant='ghost' icon={<BsThreeDots />} />
               </GridItem>
             </Grid>  
             <Divider />
           </Box>
           
-        <Box  w='85%' margin={'auto'}>
+        <Box margin={'auto'}>
           {/* Implementar funcion para crear cartas ? */}
-        <Card>
+        <Card margin={'1em'}>
+          <CardHeader>
+            <Heading  size='md'>RoboQuest</Heading>
+          </CardHeader>
+            <CardBody>
+              <Text>Pasárselo enterito</Text>
+            </CardBody>
+        </Card>
+        <Card margin={'1em'}>
           <CardHeader>
             <Heading  size='md'>RoboQuest</Heading>
           </CardHeader>
